a75c92b0a0b19d9465c841b2464d23ffdda19426
nookalamaryville <59845939+nookalamaryville@users.noreply.github.com>
2020-09-11 23:49:03 -0700

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -9,7 +9,7 @@ http.createServer(function (request, response) {
 
    // Send the response body as "Hello World"
    response.end('Welcome to Week3 Assignment.\n')
-}).listen(3003)
+}).listen(3000)
 
 // Console will print the message
 console.log('Server running')
